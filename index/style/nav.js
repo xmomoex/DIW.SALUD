34eaaa438f6eaf3305eb6f537c462a21841f8371
xmomoex <moimorex@gmail.com>
2024-02-26 19:42:34 +0100

    s

--- index/style/nav.js
+++ index/style/nav.js
@@ -4,3 +4,13 @@ function openNav() {
 function closeNav() {
   document.getElementById("menu-mobile").style.width = "0%";
 }
+
+const sr = ScrollReveal({
+  distance: "65px",
+  duration: 2600,
+  delay: 450,
+  reset: true,
+});
+
+sr.reveal(".content", { delay: 200, origin: "top" });
+sr.reveal(".boton", { delay: 200, origin: "left" });
