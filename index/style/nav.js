07054662cb96df61f8a97aabd796851bbeccdecb
xmomoex <moimorex@gmail.com>
2024-02-27 22:47:20 +0100

    S

--- index/style/nav.js
+++ index/style/nav.js
@@ -1,5 +1,5 @@
 function openNav() {
-  document.getElementById("menu-mobile").style.width = "100%";
+  document.getElementById("menu-mobile").style.width = "50%";
 }
 function closeNav() {
   document.getElementById("menu-mobile").style.width = "0%";
