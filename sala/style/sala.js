29085249f44b83c88b14c94a84e6a51b684f1e4e
xmomoex <moimorex@gmail.com>
2024-03-07 23:25:21 +0100

    s

--- sala/style/sala.js
+++ sala/style/sala.js
@@ -1,9 +1,7 @@
-// Obtener elementos del DOM
 const localVideo = document.querySelector(".local-video");
 const remoteVideo = document.querySelector(".remote-video");
 const endCallBtn = document.querySelector(".end-call-btn");
 
-// Simular videollamada (usando video local)
 navigator.mediaDevices
   .getUserMedia({ video: true, audio: true })
   .then((stream) => {
@@ -13,17 +11,11 @@ navigator.mediaDevices
     console.error("Error al obtener el stream:", error);
   });
 
-// Evento para finalizar la llamada
 endCallBtn.addEventListener("click", () => {
-  // Detener el video local
   localVideo.srcObject.getTracks().forEach((track) => {
     track.stop();
   });
-
-  // Ocultar los videos
   localVideo.style.display = "none";
   remoteVideo.style.display = "none";
-
-  // Mostrar mensaje de finalización de llamada
   alert("Llamada Finalizada");
 });
